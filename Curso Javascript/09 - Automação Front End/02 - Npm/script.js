7feded1e0d64fad58b858bfd81a95911454dd925
reveredy <carlos.andre.albanus@hotmail.com>
2022-04-28 18:15:02 -0300

    Aula de npm

---
+++ Curso Javascript/09 - Automação Front End/02 - Npm/script.js
@@ -0,0 +1,29 @@
+//NPM (Node Package Manager)
+
+//Gerenciador de pacotes
+//Feito para Node.js
+
+//Linha de comando
+
+//$ npm install lodash
+//Instala o pacote lodash no diretório atual da linha de comando
+
+//$ npm install eslint -g
+//Instala o eslint globalmente (No mac é necessário o sudo para instalar globalmente)
+
+//$ npm update lodash
+//atualiza o pacote lodash
+
+//$npm uninstall lodash
+//Desinstala o pacote lodash
+
+//Package.json
+//Arquivo local com as configurações e dependências de pacotes NPM
+
+//$npm init
+//Inicia uma nova configuração local no npm
+
+//$npm install
+//Instala todas as dependências listadas no arquivo package.json
+
+//Manter sempre todos os dados visíveis dentro das pastas

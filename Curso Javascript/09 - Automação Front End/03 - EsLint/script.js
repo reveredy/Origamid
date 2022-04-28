c1a999236d586a816533a164c99d2bfc87da6225
reveredy <carlos.andre.albanus@hotmail.com>
2022-04-28 19:58:51 -0300

    ESLint aula 1

---
+++ Curso Javascript/09 - Automação Front End/03 - EsLint/script.js
@@ -0,0 +1,12 @@
+//ESLint
+
+//Evitar problemas
+//Indica a existência de possíveis padrões problemáticos no código
+
+//Definir padrões
+//Padronizar e manter consistência entre diferentes códigos Javascript
+
+let teste = 0;
+const teste1 = 1;
+
+console.log(teste, teste1);

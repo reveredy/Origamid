30605bc5953aaf70fdc15b218afc0b2227181794
reveredy <carlos.andre.albanus@hotmail.com>
2022-04-28 22:48:42 -0300

    Foi feita a verificação de todos as funções js com o ESLint

---
+++ Curso Javascript/09 - Automação Front End/04 - EsLint 2/Animais ESLint/js/modules/fetch-bitcoin.js
@@ -0,0 +1,16 @@
+export default function initFetchBitcoin() {
+  async function fetchBTC(url) {
+    try {
+      const btcJSON = await (await fetch(url)).json();
+
+      const spanBTC = (document.querySelector('.btc-preco'));
+      spanBTC.innerText = (
+        100 / btcJSON.BRL.buy
+      ).toFixed(5);
+    } catch (erro) {
+      console.log(erro);
+    }
+  }
+
+  fetchBTC('https://blockchain.info/ticker');
+}

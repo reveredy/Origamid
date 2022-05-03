f87c10a224ad57fc16bd42c38fa6f4822156a8d8
reveredy <carlos.andre.albanus@hotmail.com>
2022-05-03 16:29:53 -0300

    Accordion Refatorado

---
+++ Curso Javascript/10 - Refatorar Projeto Final/03 - Refatorar Accordion/js/modules/navigation-tab.js
@@ -0,0 +1,23 @@
+export default function initTabNav() {
+  const tabmenu = document.querySelectorAll("[data-tab='menu'] li");
+  const tabcontent = document.querySelectorAll("[data-tab='content'] article");
+
+  function activeTab(index) {
+    tabcontent.forEach((obj) => {
+      obj.classList.remove("ativo");
+    });
+
+    const direcao = tabcontent[index].dataset.anime;
+    tabcontent[index].classList.add("ativo", direcao);
+  }
+
+  if (tabmenu.length && tabcontent.length) {
+    tabcontent[0].classList.add("ativo");
+
+    tabmenu.forEach((img, index) => {
+      img.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
---
+++ Curso Javascript/10 - Refatorar Projeto Final/03 - Refatorar Accordion/js/script.js
@@ -0,0 +1,37 @@
+import ScrollSuave from "./modules/scroll-suave.js";
+
+import initScrollAnimation from "./modules/animacao-scroll.js";
+
+import Accordion from "./modules/accordion-list.js";
+
+import initTabNav from "./modules/navigation-tab.js";
+
+import initModal from "./modules/modal.js";
+
+import initTooltip from "./modules/tooltip.js";
+
+import initDropdownMenu from "./modules/dropdown-menu.js";
+
+import initMenuMobile from "./modules/menu-mobile.js";
+
+import initFuncionamento from "./modules/funcionamento.js";
+
+import initFetchAnimais from "./modules/fetch-animais.js";
+
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
+initScrollAnimation();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();

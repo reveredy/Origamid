d1079d9b65a085f0084b3afce768aa634a926672
reveredy <carlos.andre.albanus@hotmail.com>
2022-05-03 18:59:56 -0300

    modal refatorado para class

---
+++ Curso Javascript/10 - Refatorar Projeto Final/05 - Refatorar Modal/js/script.js
@@ -0,0 +1,48 @@
+import Modal from "./modules/modal.js";
+
+import ScrollSuave from "./modules/scroll-suave.js";
+
+import Accordion from "./modules/accordion-list.js";
+
+import TabNav from "./modules/navigation-tab.js";
+
+import initTooltip from "./modules/tooltip.js";
+
+import initDropdownMenu from "./modules/dropdown-menu.js";
+
+import initMenuMobile from "./modules/menu-mobile.js";
+
+import initFuncionamento from "./modules/funcionamento.js";
+
+import initFetchAnimais from "./modules/fetch-animais.js";
+
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+
+import initScrollAnimation from "./modules/animacao-scroll.js";
+
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
+const tabNav = new TabNav(
+  "[data-tab='menu'] li",
+  "[data-tab='content'] article"
+);
+tabNav.init();
+
+const modal = new Modal(
+  '[data-modal="abrir"]',
+  '[data-modal="fechar"]',
+  '[data-modal="container"]'
+);
+modal.init();
+
+initScrollAnimation();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();

cd3531b2b3e76abd9ef4435fbfa84e07040c1e3a
reveredy <carlos.andre.albanus@hotmail.com>
2022-05-04 15:41:35 -0300

    Menu mobile refatorado com classe

---
+++ Curso Javascript/10 - Refatorar Projeto Final/13 - Refatorar menu mobile/js/modules/modal.js
@@ -0,0 +1,44 @@
+export default class Modal {
+  constructor(botaoAbrir, botaoFechar, containerModal) {
+    this.botaoAbrir = document.querySelector(botaoAbrir);
+    this.botaoFechar = document.querySelector(botaoFechar);
+    this.containerModal = document.querySelector(containerModal);
+
+    // bind this ao callback para fazer refêrencia
+    // ao objeto da classe
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.cliqueForaModal = this.cliqueForaModal.bind(this);
+  }
+
+  // abre ou fecha o modal
+  toggleModal() {
+    this.containerModal.classList.toggle("ativo");
+  }
+
+  // Fecha o modal ao clicar fora do container
+  cliqueForaModal(event) {
+    if (event.target === this.containerModal) {
+      this.toggleModal();
+    }
+  }
+
+  // adiciona os eventos aos elementos do modal
+  addModalEvents() {
+    this.botaoAbrir.addEventListener("click", this.eventToggleModal);
+    this.botaoFechar.addEventListener("click", this.eventToggleModal);
+    this.containerModal.addEventListener("click", this.cliqueForaModal);
+  }
+
+  // adiciona o evento de toggle ao modal
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
+  }
+
+  init() {
+    if (this.containerModal && this.botaoAbrir && this.botaoFechar) {
+      this.addModalEvents();
+    }
+    return this;
+  }
+}
